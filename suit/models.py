5a9dcf7a129a8e322bf6cb4ceb7f661af9b5ce30
Andres G. Quina <andresgqjob@gmail.com>
2020-12-16 20:24:07 +0100

    brand & product suit model

--- suit/models.py
+++ suit/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Brand(models.Model):
+    name          = models.CharField(max_length=50)
+    created_at    = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name          = models.CharField(max_length=50)
+    price         = models.IntegerField()
+    description   = models.TextField()
+    new           = models.BooleanField()
+    brand         = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    fabr_date     = models.DateField()
+    created_at    = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
